3d21b63eeae23c7b93ebd6062d8caa8c97926dea
Bo Duan <boduan402@gmail.com>
2020-05-31 11:05:02 -0700

    Add solution for TwoSumIV.

---
+++ src/leetcode/P653_TwoSumIV.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+import util.TreeNode;
+
+public class P653_TwoSumIV {
+
+  public static void main(String[] args) {
+    TreeNode root = TreeNode.SAMPLE_0;
+    P653_TwoSumIV solution = new P653_TwoSumIV();
+    System.out.println(solution.findTarget(root, 17));
+  }
+
+  private final Set<Integer> set = new HashSet<>();
+
+  private boolean findTarget(TreeNode root, int k) {
+    if (root == null) {
+      return false;
+    }
+
+    int val = root.val;
+    if (set.contains(k - val)) {
+      return true;
+    } else {
+      set.add(val);
+    }
+
+    return findTarget(root.left, k) || findTarget(root.right, k);
+  }
+}
